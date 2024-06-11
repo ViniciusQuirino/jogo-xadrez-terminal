8a81f388ff36854f9f98c2a6f60e9825cf7d867e
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-06-11 10:15:00 -0300

    Melhorando a implementação do metedo imprimirTabuleiro

--- Tela.cs
+++ Tela.cs
@@ -11,6 +11,7 @@ namespace jogo_xadrez_terminal
 
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -19,10 +20,27 @@ namespace jogo_xadrez_terminal
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.Write("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor; //pega a cor do sistema "branco" e salva em aux
+                Console.ForegroundColor = ConsoleColor.Yellow; //coloca amarelo na peça
+                Console.Write(peca); //imprime
+                Console.ForegroundColor = aux; //volta a peça na cor do sistema
             }
         }
     }
